b794d848614eb890304cfaa98a95edb1d3d6a0e8
MichaelMcFaddenjr <michaelpmcfaddenjr@gmail.com>
2021-03-18 15:12:35 -0700

    michael-mcfadden

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import axios from 'axios';
+import Header from './components/header';
+import Image from './components/image';
+import Placard from './components/placard';
+
 
 function App() {
+  const [nasaInfo, setNasaInfo] = useState({})
+
+  useEffect( () => {
+    axios
+    .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    .then((res) => {
+      setNasaInfo(res.data)
+    })
+    .catch((res) => {
+      console.log(res)
+    })
+  },[])
+
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+    <div className='App'>
+      <Header nasaInfo = {nasaInfo}/>
+      <Image nasaInfo = {nasaInfo}/>
+      <Placard nasaInfo = {nasaInfo}/>
+      {/* styled components pull*/}
     </div>
-  );
+  )
 }
 
 export default App;
